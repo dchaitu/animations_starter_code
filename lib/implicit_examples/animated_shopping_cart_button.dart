b5544cebfcc4e01f1567671a5f8d224f142dfe7f
dchaitu <chaitanyadokara@gmail.com>
2025-02-07 22:22:45 +0530

    completed implicit animation part 1

--- lib/implicit_examples/animated_shopping_cart_button.dart
+++ lib/implicit_examples/animated_shopping_cart_button.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class ShoppingCartButton extends StatelessWidget {
+class ShoppingCartButton extends StatefulWidget {
   const ShoppingCartButton({super.key});
+
+  @override
+  State<ShoppingCartButton> createState() => _ShoppingCartButtonState();
+}
+
+class _ShoppingCartButtonState extends State<ShoppingCartButton> {
+  Color selectedColor = Colors.green;
+  bool isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -10,19 +18,36 @@ class ShoppingCartButton extends StatelessWidget {
         title: const Text('Shopping Cart'),
       ),
       body: Center(
-        child: Container(
-          width: 80.0,
-          height: 60.0,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: const Icon(
-            Icons.shopping_cart,
-            color: Colors.white,
-          ),
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              isExpanded =!isExpanded;
+            });
+          },
+          child: AnimatedContainer(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            width: isExpanded? 180.0: 80,
+            height: 60.0,
+            decoration: BoxDecoration(
+              color: isExpanded? selectedColor:Colors.blue,
+              borderRadius: BorderRadius.circular(isExpanded?30:10.0),
+            ),
+            duration: Duration(milliseconds: 500),
+            child:  Row(
+              children: [
+                Align(
+                  alignment: isExpanded?Alignment.centerLeft: Alignment.center,
+                  child: Icon(
+                    isExpanded?Icons.check:Icons.shopping_cart,
+                      color: Colors.white,
+                    ),
+                ),
+                isExpanded?Expanded(child: Text("Added to cart",style: TextStyle(color: Colors.white),)):Expanded(child: SizedBox())
+              ],
+            ),
+            ),
         ),
-      ),
+        ),
     );
   }
 }
