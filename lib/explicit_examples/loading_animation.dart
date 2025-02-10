61130b786bc0642048f3e9018dd980121cbe701c
dchaitu <chaitanyadokara@gmail.com>
2025-02-10 21:42:25 +0530

    completed  explicit animation

--- lib/explicit_examples/loading_animation.dart
+++ lib/explicit_examples/loading_animation.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class RadialProgressAnimation extends StatelessWidget {
+class RadialProgressAnimation extends StatefulWidget {
   final double progress;
   final Color color;
 
@@ -11,34 +11,65 @@ class RadialProgressAnimation extends StatelessWidget {
   });
 
   @override
+  State<RadialProgressAnimation> createState() => _RadialProgressAnimationState();
+}
+
+class _RadialProgressAnimationState extends State<RadialProgressAnimation> with SingleTickerProviderStateMixin{
+
+
+  late AnimationController animationController;
+  late Animation animationProgress;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 1000));
+    animationProgress = Tween<double>(begin: 0 , end: widget.progress).animate(animationController);
+
+  }
+
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            SizedBox(
-              width: 150,
-              height: 150,
-              child: CircularProgressIndicator(
-                value: progress,
-                strokeWidth: 10,
-                backgroundColor: Colors.grey.shade100,
-                color: color,
-              ),
-            ),
-            Text(
-              '${(progress * 100).toInt()}%',
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
+        child: AnimatedBuilder(
+          animation: animationProgress,
+          builder: (BuildContext context, Widget? child) {
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: CircularProgressIndicator(
+                    value: animationProgress.value,
+                    strokeWidth: 10,
+                    backgroundColor: Colors.grey.shade100,
+                    color: widget.color,
+                  ),
+                ),
+                Text(
+                  '${(animationProgress.value * 100).toInt()}%',
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            );
+          },
+
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          if(animationController.isCompleted) {
+            animationController.reverse();
+          } else {
+            animationController.forward();
+          }
+        },
         child: const Icon(Icons.start),
       ),
     );
