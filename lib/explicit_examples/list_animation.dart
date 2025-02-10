fdfa3de5d51d78db7f7ec6e5014bc45659f92f99
dchaitu <chaitanyadokara@gmail.com>
2025-02-10 19:23:14 +0530

    completed adding interval to explicit animation

--- lib/explicit_examples/list_animation.dart
+++ lib/explicit_examples/list_animation.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-class ListAnimation extends StatelessWidget {
+class ListAnimation extends StatefulWidget {
   const ListAnimation({super.key});
+
+  @override
+  State<ListAnimation> createState() => _ListAnimationState();
+}
+
+class _ListAnimationState extends State<ListAnimation> with SingleTickerProviderStateMixin {
+  int listItemsCount = 5;
+  late AnimationController animationController;
+
+  late List<Animation<Offset>> tilesSlideAnimation = [];
+
+  @override
+  void initState() {
+
+    super.initState();
+    animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 5000));
+    // tilesSlideAnimation = Tween<Offset>(begin: Offset(-1, 1), end: Offset(0, 1)).animate(animationController);
+
+    // animationController.forward();
+    tilesSlideAnimation = List.generate(listItemsCount,
+            (index) => Tween<Offset>(begin: Offset(-1, 1), end: Offset(0, 1)).
+            animate(
+        CurvedAnimation(
+            parent: animationController,
+            curve: Interval((index/listItemsCount).toDouble(), 1.0)),
+            ));
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +38,25 @@ class ListAnimation extends StatelessWidget {
         title: const Text('List Animation'),
       ),
       body: ListView.builder(
-        itemCount: 5,
+        itemCount: listItemsCount,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Hello World, Rivaan. ${index.toString()}'),
+          return SlideTransition(
+            position: tilesSlideAnimation[index],
+            child: ListTile(
+              title: Text('Hello World, Rivaan. ${index.toString()}'),
+            ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          if(animationController.isCompleted) {
+            animationController.reverse();
+          }
+          else {
+            animationController.forward();
+          }
+        },
         child: const Icon(Icons.done),
       ),
     );
